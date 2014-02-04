299e20dcc5616b8e19633b6024826b56bbbe853f
Jason Foutz <jfoutz@gmail.com>
2014-02-03 20:33:16 -0700

    ignore .vagrant file

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+yum -y install git
+rpm -i /vagrant/resources/jdk-7u51-linux-x64.rpm
+mkdir -p /usr/local/stash
+tar -zxvf /vagrant/resources/atlassian-stash-2.10.1.tar.gz -C /usr/local/stash
